addc249d227141f6dd336203cd0ca94584d25da0
Pedrocaloi <caloieropedro@gmail.com>
2023-08-14 18:16:26 -0300

    table component created

--- packages/frontend/src/app/dashboard/page.tsx
+++ packages/frontend/src/app/dashboard/page.tsx
@@ -1,22 +1,25 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography } from '@mui/material';
+import SuscriptionTable from '@/components/suscriptionTable';
 
 export default function Dashboard() {
-    return (
-        <Container
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            }}
-        >
-            <Box>
-                <Typography variant="h4" component="h4" gutterBottom>
-                    Dashboard
-                </Typography>
-            </Box>
+ return (
+  <Container
+   sx={{
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+   }}>
+   <Box>
+    <Typography
+     variant='h4'
+     component='h4'
+     gutterBottom>
+     Dashboard
+    </Typography>
+   </Box>
 
-            {/* table */}
-            
-        </Container>
-    )
-};
+   {/* table */}
+   <SuscriptionTable />
+  </Container>
+ );
+}
