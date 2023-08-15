d87a2ca659b0ce0f00660783a848baaccbef555d
valentinnavalos <valentinnavalos@gmail.com>
2023-08-14 22:36:29 -0300

    fix: header layout

--- packages/frontend/src/layouts/sidebarLayout/index.tsx
+++ packages/frontend/src/layouts/sidebarLayout/index.tsx
@@ -1,9 +1,12 @@
+'use client'
+
 import { FC, ReactNode } from "react";
 import { Box } from "@mui/material";
 import PropTypes from "prop-types";
 
-import Sidebar from "./sidebar";
 import useSidebar from "@/hooks/useSidebar";
+import Sidebar from "./sidebar";
+import Header from "./header";
 
 interface SideBarLayoutProps {
     children?: ReactNode;
@@ -13,6 +16,7 @@ const SideBarLayout: FC<SideBarLayoutProps> = ({ children }) => {
     const { sidebarToggle } = useSidebar();
     return (
         <>
+            <Header />
             <Sidebar />
             <Box
                 sx={{
@@ -24,6 +28,7 @@ const SideBarLayout: FC<SideBarLayoutProps> = ({ children }) => {
                         xs: sidebarToggle ? '240px' : 0,
                         lg: '240px',
                     },
+                    marginTop: '80px',
                 }}
             >
                 <Box display={'block'}>
