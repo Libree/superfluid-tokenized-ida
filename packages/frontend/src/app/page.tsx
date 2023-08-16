0e467c8c7cea52c86d820912fbc22038354f061c
Pedrocaloi <caloieropedro@gmail.com>
2023-08-16 11:21:29 -0300

    todo ok

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 import { Container } from '@mui/material';
-import SuscriptionTable from '@/components/subscriptionTable';
-=======
-import { Container } from "@mui/material";
-import SubscriptionTable from "@/components/subscriptionTable";
+import SubscriptionTable from '@/components/subscriptionTable';
 
 export default function Home() {
-  return (
-    <Container>
-      <SubscriptionTable />
-    </Container>
-  );
+ return (
+  <Container>
+   <SubscriptionTable />
+  </Container>
+ );
 }
