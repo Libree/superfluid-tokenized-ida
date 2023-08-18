0acc6390cae0664dfe65a472585df957db3aca74
Pedrocaloi <caloieropedro@gmail.com>
2023-08-18 09:34:25 -0300

    chart not working yet

--- packages/frontend/src/components/analitycChart/index.tsx
+++ packages/frontend/src/components/analitycChart/index.tsx
@@ -13,53 +13,53 @@ import {
  Filler,
 } from 'chart.js';
 
+ChartJS.register(
+ CategoryScale,
+ LinearScale,
+ BarElement,
+ Title,
+ Tooltip,
+ Legend,
+ Filler,
+);
+
+const data = {
+ labels: [1950, 1960, 1970, 1980, 1990, 2000, 2010],
+ datasets: [
+  {
+   label: 'Your Subscriptions',
+   data: [2.5, 3.0, 2.8, 4.2, 5.5, 4.8, 6.0],
+   backgroundColor: 'rgba(53, 162, 235, 0.4)',
+   borderColor: 'rgb(53, 162, 235)',
+   borderWidth: 1,
+  },
+ ],
+};
+
+const options = {
+ responsive: true,
+ plugins: {
+  legend: {
+   display: false,
+  },
+ },
+ scales: {
+  y: {
+   beginAtZero: true,
+   stepSize: 0.5,
+   max: 6.5,
+  },
+  x: {
+   ticks: { color: 'rgba(0,220,195)' },
+  },
+ },
+ title: {
+  display: true,
+  text: 'Your subscriptions',
+ },
+};
+
 const AnalitycChart = () => {
- ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler,
- );
-
- const data = {
-  labels: ['1950', '1960', '1970', '1980', '1990', '2000', '2010'],
-  datasets: [
-   {
-    label: 'Your Subscriptions',
-    data: [2.5, 3.0, 2.8, 4.2, 5.5, 4.8, 6.0],
-    backgroundColor: 'white',
-    borderColor: 'black',
-    borderWidth: 1,
-   },
-  ],
- };
-
- const options = {
-  responsive: true,
-  plugins: {
-   legend: {
-    display: false,
-   },
-  },
-  scales: {
-   y: {
-    beginAtZero: true,
-    stepSize: 0.5,
-    max: 6.5,
-   },
-   x: {
-    ticks: { color: 'rgba(0,220,195)' },
-   },
-  },
-  title: {
-   display: true,
-   text: 'Chart.js Bar Chart',
-  },
- };
-
  return (
   <Box>
    <Typography
@@ -68,12 +68,10 @@ const AnalitycChart = () => {
     marginTop='3em'>
     Your subscribers
    </Typography>
-   <Box>
-    <Bar
-     data={data}
-     options={options}
-    />
-   </Box>
+   <Bar
+    data={data}
+    options={options}
+   />
   </Box>
  );
 };
