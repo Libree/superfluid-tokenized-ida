677fe784e4c725b66cee2974aef0abbcd1c9a0f6
valentinnavalos <valentinnavalos@gmail.com>
2023-08-19 11:56:53 -0300

    feat: connect wallet

--- packages/frontend/src/components/connectWallet/index.tsx
+++ packages/frontend/src/components/connectWallet/index.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { Box, Button } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
 
 import { useAccount, useConnect, useEnsName } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
+import { shortenAddress } from "@/utils";
 
 export const ConnectWallet = () => {
     const { address, isConnected } = useAccount();
@@ -13,36 +14,45 @@ export const ConnectWallet = () => {
         connector: new InjectedConnector(),
     });
 
-    if (isConnected)
-        return (
-            <Box sx={{ margin: '1rem 0' }}>
-                <div>Connected to {ensName ?? address}</div>
-            </Box>
-        )
-
     return (
         <Box sx={{ margin: '1rem 0' }}>
-            <Button
-                LinkComponent={"a"}
-                startIcon={<AccountBalanceWalletOutlinedIcon />}
-                sx={{
-                    alignItems: 'left',
-                    fontWeight: 'bold',
-                    textTransform: 'capitalize',
-                    width: '100%',
-                    backgroundColor: 'green',
-                    color: 'white',
-                    border: '1px solid transparent',
-                    borderRadius: '5rem',
-                    '&:hover': {
-                        border: '1px solid green',
-                        color: 'green',
-                    },
-                }}
-                onClick={() => connect()}
-            >
-                Connect Wallet
-            </Button>
-        </Box>
+            {isConnected ? (
+                <Typography
+                    sx={{
+                        backgroundColor: 'rgba(105, 255, 102, 0.3)',
+                        color: 'rgba(45, 105, 40, 1)',
+                        padding: '0.2em',
+                        borderRadius: '0.5em',
+                        display: 'inline-block',
+                        fontWeight: 'bold',
+                        fontSize: '0.9em',
+                    }}
+                >
+                    Connected to {ensName ?? shortenAddress(address as string)}
+                </Typography>
+            ) : (
+                <Button
+                    LinkComponent={"a"}
+                    startIcon={<AccountBalanceWalletOutlinedIcon />}
+                    sx={{
+                        alignItems: 'left',
+                        fontWeight: 'bold',
+                        textTransform: 'capitalize',
+                        width: '100%',
+                        backgroundColor: 'green',
+                        color: 'white',
+                        border: '1px solid transparent',
+                        borderRadius: '5rem',
+                        '&:hover': {
+                            border: '1px solid green',
+                            color: 'green',
+                        },
+                    }}
+                    onClick={() => connect()}
+                >
+                    Connect Wallet
+                </Button>
+            )}
+        </Box >
     )
 };
