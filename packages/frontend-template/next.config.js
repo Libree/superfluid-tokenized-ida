78dec52fa8a5dcdec98c5ab1e8a66061284ed1ac
valentinnavalos <valentinnavalos@gmail.com>
2023-08-23 16:26:14 -0300

    fix: enable ts at build

--- packages/frontend-template/next.config.js
+++ packages/frontend-template/next.config.js
@@ -5,4 +5,11 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
