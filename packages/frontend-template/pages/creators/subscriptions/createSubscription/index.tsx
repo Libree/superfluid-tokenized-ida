358652e8893edb12541c69a029af2488e61a3d0b
Pedrocaloi <caloieropedro@gmail.com>
2023-08-24 15:28:38 -0300

    refactor: path refactor

---
+++ packages/frontend-template/pages/creators/subscriptions/createSubscription/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PageContainer from '../../../../src/components/container/PageContainer';
+import Breadcrumb from '../../../../src/layouts/full/shared/breadcrumb/Breadcrumb';
+import SubscriptionForm from '../../../../src/components/forms/subscription-form';
+
+const CreateSubscription = () => {
+ return (
+  <PageContainer>
+   <Breadcrumb
+    title='Create Subscription'
+    subtitle='Create your custom subscription'
+   />
+   <SubscriptionForm />
+  </PageContainer>
+ );
+};
+
+export default CreateSubscription;
