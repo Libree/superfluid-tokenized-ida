034912f9e95194a907487bdd7021dbac8f60ab56
valentinnavalos <valentinnavalos@gmail.com>
2023-08-29 11:29:32 -0300

    style: chain imgs

---
+++ packages/frontend-template/utils/index.ts
@@ -0,0 +1,10 @@
+import EthereumIcon from '../public/images/chains/ethereum-icon.png';
+import PolygonIcon from '../public/images/chains/polygon-icon.png';
+import GoerliIcon from '../public/images/chains/goerli-icon.png';
+
+export const NETWORKS_ICON = {
+    'homestead': EthereumIcon,
+    'matic': PolygonIcon,
+    'goerli': GoerliIcon,
+    'maticmum': PolygonIcon,
+};
