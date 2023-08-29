4e8ef2a8ff538f494f0ba266413b9b6cc64b8bfe
valentinnavalos <valentinnavalos@gmail.com>
2023-08-29 10:42:30 -0300

    feat: change network

--- packages/frontend-template/src/layouts/full/vertical/header/NetworkSelect.tsx
+++ packages/frontend-template/src/layouts/full/vertical/header/NetworkSelect.tsx
@@ -1,34 +1,49 @@
 import React from 'react';
 import {
-    Box, MenuItem, Select,
+    Box, CircularProgress, MenuItem, Select,
 } from '@mui/material';
 import { useNetwork, useSwitchNetwork } from 'wagmi';
 
 
 const NetworkSelect = () => {
     const { chain } = useNetwork();
-    const { chains } = useSwitchNetwork();
+    const { chains, isLoading, switchNetwork } = useSwitchNetwork();
 
-    const handleNetworkSelect = (event: any) => {};
+    const handleNetworkSelect = (event: any) => {
+        switchNetwork?.(event.target.value);
+    };
 
     return (
         <Box>
-            <Select
-                label='Network'
-                value={chain?.id}
-                onChange={handleNetworkSelect}
-                placeholder={chain?.name}
-            >
-                {chains.map((network) => (
-                    <MenuItem
-                        key={network.id}
-                        value={network.id}
-                    >
-                        {network.name}
-                    </MenuItem>
-                ))}
-            </Select>
-        </Box>
+            {isLoading ? (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: 120,
+                        height: 50,
+                    }}
+                >
+                    <CircularProgress />
+                </Box>
+            ) : (
+                <Select
+                    value={chain?.id}
+                    onChange={handleNetworkSelect}
+                    placeholder={chain?.name}
+                >
+                    {chains.map((network) => (
+                        <MenuItem
+                            key={network.id}
+                            value={network.id}
+                        >
+                            {network.name}
+                        </MenuItem>
+                    ))}
+                </Select>
+            )}
+        </Box >
     );
 };
 
