98a1c91512f287c266eecfb112ba10c0c7ca3030
Ivan <ivan.alberquilla@gmail.com>
2023-08-29 18:23:40 +0200

    subscription manager

---
+++ packages/frontend-template/hooks/useSubscriptionManager.tsx
@@ -0,0 +1,53 @@
+import { useAccount, useNetwork, useContractRead, useContractWrite, useWaitForTransaction } from "wagmi";
+import { SubscriptionManager__factory } from "../src/typechain/SubscriptionManager__factory";
+import { DEPLOYED_CONTRACTS } from "../src/utils/constants/contracts";
+
+export function useSubscriptionManager() {
+
+    const { address } = useAccount();
+    const { chain } = useNetwork()
+
+    const {
+        data: subscriptions,
+        isError: isErrorSubscriptions,
+        isLoading: isLoadingSubscriptions
+    } = useContractRead({
+        address: DEPLOYED_CONTRACTS[chain?.id].manager,
+        abi: SubscriptionManager__factory.abi,
+        functionName: 'getSubscriptions',
+        args: [address]
+    })
+
+    const {
+        data: create,
+        write: createSubscriptionTx,
+        isLoading: isCreateLoading,
+        isSuccess: isCreateStarted,
+        error: createError,
+    } = useContractWrite({
+        address: DEPLOYED_CONTRACTS[chain?.id].manager,
+        abi: SubscriptionManager__factory.abi,
+        functionName: "createSubscription",
+    });
+
+    const { isSuccess: txCreateSuccess, error: txCreateError } = useWaitForTransaction({
+        confirmations: 1,
+        hash: create?.hash,
+    });
+
+    const createSubscription = (paymentToken: string, flowRate: number, metadata: string) => {
+        createSubscriptionTx({ args: [paymentToken, flowRate, ethers.utils.formatBytes32String(metadata)] })
+    }
+
+    return {
+        subscriptions,
+        isErrorSubscriptions,
+        isLoadingSubscriptions,
+        isCreateLoading,
+        isCreateStarted,
+        createError,
+        txCreateSuccess,
+        txCreateError,
+        createSubscription
+    };
+};
