6459ad059b59297dc9748348e3a4d6fc8e65a93e
Ivan <ivan.alberquilla@gmail.com>
2023-08-30 16:12:11 +0200

    streams data

--- packages/frontend-template/hooks/useSuperfluidData.tsx
+++ packages/frontend-template/hooks/useSuperfluidData.tsx
@@ -1,17 +1,34 @@
-import { ApolloClient } from '@apollo/client';
-import { DATA_QUERY } from '../src/utils/constants/query';
+import { ApolloClient, NormalizedCacheObject, InMemoryCache } from '@apollo/client';
+import { useAccount } from "wagmi";
+import { SUPERFLUID_STREAMS } from '../src/utils/constants/query';
+import { SUBGRAPH_SUPERFLUID_API_URL } from '../src/utils/constants/api';
+import { useEffect, useState } from 'react';
 
 export async function useSuperfluidData() {
-    const getStreams = async (client: ApolloClient<object>) => {
-        const { data, error } = await client.query({
-            query: DATA_QUERY,
-        });
 
-        if (!error) return data
-        console.error('Error fetching token data', error);
-    }
+    const { address } = useAccount();
+    const [streams, setStreams] = useState()
+
+    useEffect(() => {
+        async function loadData() {
+            const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+                cache: new InMemoryCache(),
+                uri: SUBGRAPH_SUPERFLUID_API_URL['mumbai']
+            });
+
+            const { data, error } = await client.query({
+                query: SUPERFLUID_STREAMS,
+                variables: { user: address }
+            });
+
+            if(!error) setStreams(data)
+        }
+
+        loadData()
+
+    }, [])
 
     return {
-        getStreams
+        streams
     }
 };
