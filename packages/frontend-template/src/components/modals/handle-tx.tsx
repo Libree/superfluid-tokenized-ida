c1df90eb761ec4aa59f15f4a243ba0780c781e87
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2023-08-31 15:42:05 +0000

    style: basic modal

---
+++ packages/frontend-template/src/components/modals/handle-tx.tsx
@@ -0,0 +1,36 @@
+import { Box, Modal, Typography } from "@mui/material";
+import { useGlobalModalsContext } from "../../context/globalModals";
+
+const modalContentStyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 500,
+    bgcolor: 'black',
+    border: '2px solid #000',
+    borderRadius: '1rem',
+    boxShadow: 24,
+    p: 4,
+}
+
+export const HandleTxModal = () => {
+    const { isHandleTxOpen, close } = useGlobalModalsContext();
+
+    return (
+        <>
+            <Modal
+                open={isHandleTxOpen}
+                onClose={() => close()}
+            >
+                <Box
+                    sx={modalContentStyle}
+                >
+                    <Typography variant="h6" component="h2">
+                        Tx should be approved
+                    </Typography>
+                </Box>
+            </Modal>
+        </>
+    )
+};
