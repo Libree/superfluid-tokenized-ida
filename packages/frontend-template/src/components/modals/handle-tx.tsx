53e5fc343a4c1156b92b39bc0aef211b46374463
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2023-08-31 18:32:25 +0000

    fix: handle tx redirect btn

--- packages/frontend-template/src/components/modals/handle-tx.tsx
+++ packages/frontend-template/src/components/modals/handle-tx.tsx
@@ -1,5 +1,6 @@
-import { Box, CircularProgress, Modal, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, Divider, Modal, Typography } from "@mui/material";
 import { useGlobalModalsContext } from "../../context/globalModals";
+import Link from "next/link";
 
 const modalContentStyle = {
     position: 'absolute' as 'absolute',
@@ -12,6 +13,10 @@ const modalContentStyle = {
     borderRadius: '1rem',
     boxShadow: 24,
     p: 4,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
 }
 
 type HandleTxModalProps = {
@@ -55,14 +60,32 @@ export const HandleTxModal = ({
                         </Box>
                     )}
                     {isSuccess && (
-                        <Typography variant="h6" component="h2">
-                            Success tx
-                        </Typography>
+                        <>
+                            <Typography variant="h6" component="h2">
+                                Success tx
+                            </Typography>
+                            <Divider sx={{ marginY: '0.3rem' }} />
+                            <Button
+                                component={Link}
+                                href={`/creators/subscriptions}`}
+                            >
+                                Accept
+                            </Button>
+                        </>
                     )}
                     {isError && (
-                        <Typography variant="h6" component="h2">
-                            An error occured executing the tx
-                        </Typography>
+                        <>
+                            <Typography variant="h6" component="h2">
+                                An error occured executing the tx
+                            </Typography>
+                            <Divider sx={{ marginY: '0.3rem' }} />
+                            <Button 
+                                component={Link}
+                                href={`/creators/subscriptions}`}
+                            >
+                                Accept
+                            </Button>
+                        </>
                     )}
                 </Box>
             </Modal>
